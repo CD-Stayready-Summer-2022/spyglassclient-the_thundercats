95a8ae3c09a68eba269289de05d0dff167eb8019
JasmineCodes-New <jasmine.irvin@ymail.com>
2022-08-03 14:36:53 -0400

    Dashboard, SignUp, SignIn basic setup

---
+++ src/pages/SignIn.js
@@ -0,0 +1,9 @@
+import * as React from 'react'; 
+
+export function SignIn() {
+    return(
+        <>
+            <h1>Sign In</h1>
+        </>
+    )
+}
---
+++ src/pages/SignUp.js
@@ -0,0 +1,9 @@
+import * as React from 'react'; 
+
+export function SignUp() {
+    return(
+        <>
+            <h1>Sign Up</h1>
+        </>
+    )
+}
