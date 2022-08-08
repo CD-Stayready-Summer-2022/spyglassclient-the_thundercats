a10463d47f16e0738bdb1e66efdd9bc7a965c856
BwinaRose <sabrinakrueger24@gmail.com>
2022-08-08 11:59:43 -0400

    working user api functions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { SignUp } from './pages/SignUp';
 import { ForgotPassword } from './pages/page-links/ForgotPassword';
 import { SeeAllGoals } from './pages/SeeAllGoals';
 import { UpdateGoal } from './pages/UpdateGoal';
+import { TestFunctions } from './pages/TestFunctions';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import(ForgotPassword);
@@ -21,6 +22,7 @@ function App() {
                     <Route path="/forgotpassword" element={<ForgotPassword />} />
                     <Route path="/allgoals" element={<SeeAllGoals />} />
                     <Route path="/updategoal" element={<UpdateGoal />} />
+                    <Route path="/test" element={<TestFunctions />} />
                 </Routes>
             </BrowserRouter>
         </>
