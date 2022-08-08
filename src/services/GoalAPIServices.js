2ffedc8acc1a430960fcd1ec6fca26f0806105f1
McKenna <ohara10@purdue.edu>
2022-08-08 16:04:10 -0500

    create goal works

--- src/services/GoalAPIServices.js
+++ src/services/GoalAPIServices.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 const GOAL_BASE_API_URI = "http://localhost:8080/api/v1/recurringgoals";
 
+const token = sessionStorage.getItem("authToken");
+
+const config = {
+    headers: { Authorization: `Bearer ${token}` }
+};
+
 export function createGoal(goal, userId) {
-  return axios.post(`${GOAL_BASE_API_URI}/create${userId}`, goal);
+  return axios.post(`${GOAL_BASE_API_URI}/${userId}`, goal, config);
 }
 export function getGoalById(id) {
   return axios.get(`${GOAL_BASE_API_URI}/${id}`);
